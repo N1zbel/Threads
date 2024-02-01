8b7b32281fee264fea4597378a7b3b988a1321d3
Andrey Ryazanov <ark2635@gmail.com>
2024-02-01 19:27:50 +0300

    upd views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import UserProfile, Post, Comment
 from .serializers import UserProfileSerializer, PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrAdminPermission
 
 
 class UserProfileListCreateView(generics.ListCreateAPIView):
@@ -8,11 +9,31 @@ class UserProfileListCreateView(generics.ListCreateAPIView):
     serializer_class = UserProfileSerializer
 
 
+class UserProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsOwnerOrAdminPermission]
+
+
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrAdminPermission]
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrAdminPermission]
