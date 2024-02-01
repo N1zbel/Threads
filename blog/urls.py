1539a3a26e8303a704fd706e2840bd677683d8da
Andrey Ryazanov <ark2635@gmail.com>
2024-02-01 19:27:39 +0300

    upd urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import UserProfileListCreateView, PostListCreateView, CommentListCreateView
+from .views import UserProfileListCreateView, UserProfileDetailView, \
+    PostListCreateView, PostDetailView, \
+    CommentListCreateView, CommentDetailView
 
 urlpatterns = [
     path('userprofiles/', UserProfileListCreateView.as_view(), name='userprofile-list-create'),
+    path('userprofiles/<int:pk>/', UserProfileDetailView.as_view(), name='userprofile-detail'),
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
 ]
