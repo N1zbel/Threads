707f5e59e82b15fe6e46198c95dc47ea724858eb
Andrey Ryazanov <ark2635@gmail.com>
2024-02-01 20:32:02 +0300

    upd admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import UserProfile, Post, Comment
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('created_at',)
+
+
 admin.site.register(UserProfile)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
