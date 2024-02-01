fc0b8b9a15ae79410093923c48744cceafcb90da
Andrey Ryazanov <ark2635@gmail.com>
2024-02-01 19:48:29 +0300

    upd models

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,6 @@ class Comment(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author.username}: {self.text}'
